f413c8228fdaa0ab6fc558e3f29e57fc9b7450b1
James W Johnson <me@thejameswilliam.com>
2023-12-28 09:59:29 -0700

    added badge list block.

--- inc/blocks.php
+++ inc/blocks.php
@@ -60,6 +60,116 @@ add_action('acf/init', function () {
 		);
 
 
+		acf_register_block_type(array(
+			'name'              => 'make-badge-list',
+			'title'             => __('Badge List'),
+			'description'       => __('A block that displays all the Badges in a card-list format.'),
+			'render_template'   => MAKESF_ABSPATH . '/inc/templates/make-badge-list.php',
+			'category'          => 'make-blocks',
+			// 'icon'              => file_get_contents(MAKESF_URL . 'inc/img/logo-sm.svg'),
+			'keywords'          => array( 'badge', 'list', 'diciplines', 'make', 'mind', 'Mindshare'),
+			'align'             => 'full',
+			'mode'            	=> 'edit',
+			'multiple'          => false,
+			'supports'					=> array(
+				'align' => false,
+			),
+			'enqueue_assets' => function(){
+				// We're just registering it here and then with the action get_footer we'll enqueue it.
+				wp_register_style( 'make-block-styles', MAKESF_URL . 'css/style.css', array(),  MAKESF_PLUGIN_VERSION);
+				add_action( 'get_footer', function () {wp_enqueue_style('make-block-styles');});
+
+
+			})
+		);
+
+
 
 	endif;
 });
+
+
+
+
+
+
+
+add_action( 'acf/include_fields', function() {
+	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+		return;
+	}
+
+	acf_add_local_field_group( array(
+		'key' => 'group_658da62ab6c8a',
+		'title' => 'Block: Badge List',
+		'fields' => array(
+			array(
+				'key' => 'field_658da62b7a6d1',
+				'label' => 'Make Badge List',
+				'name' => 'make_badge_list',
+				'aria-label' => '',
+				'type' => 'group',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'layout' => 'block',
+				'sub_fields' => array(
+					array(
+						'key' => 'field_658da63e7a6d2',
+						'label' => 'Badges',
+						'name' => 'badges',
+						'aria-label' => '',
+						'type' => 'relationship',
+						'instructions' => 'If nothing is selected all Badges will display.',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'post_type' => array(
+							0 => 'certs',
+						),
+						'post_status' => '',
+						'taxonomy' => '',
+						'filters' => array(
+							0 => 'search',
+						),
+						'return_format' => 'id',
+						'min' => '',
+						'max' => '',
+						'elements' => '',
+						'bidirectional' => 0,
+						'bidirectional_target' => array(
+						),
+					),
+				),
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'block',
+					'operator' => '==',
+					'value' => 'acf/make-badge-list',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	) );
+} );
+
---
+++ inc/templates/make-badge-list.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * MAKE Member Sign In
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $r_post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'make-badge-list-' . $block['id'];
+if( !empty($block['anchor']) ) {
+    $id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$className = 'make-badge-list';
+if( !empty($block['className']) ) {
+  $className .= ' ' . $block['className'];
+
+}
+if( !empty($block['align']) ) {
+  $className .= ' align' . $block['align'];
+}
+
+
+$make_badge_list = get_field('make_badge_list');
+$badges = $make_badge_list['badges'];
+$badge_args = array(
+  'post_type' => 'certs',
+  'posts_per_page' => -1
+);
+
+if($badges) :
+  $badge_args['post__in'] = $badges;
+  $badge_args['orderby'] = 'post__in';
+endif;
+
+
+$badge_query = new WP_Query($badge_args);
+if($badge_query->have_posts()) :
+
+echo '<div class="' . $className . '" id= "' . $id .'">';
+    echo '<div class="row gy-2">';
+    while($badge_query->have_posts()) :
+        $badge_query->the_post();
+
+        $badge_icon = wp_get_attachment_image( get_field('badge_image', get_the_id()), 'thumbnail', false );
+
+        echo '<div class="col-6 col-md-4 col-lg-3">';
+            echo '<div class="card p-2 badge-card text-center d-flex flex-column justify-content-center h-100">';
+                echo ($badge_icon ? '<div class="badge-image">' . $badge_icon . '</div>' : null);
+                echo '<div class="badge__title">';
+                    echo '<a href="' . get_the_permalink() . '">';
+                        echo '<h3 class="h5 text-center">' . get_the_title() . '</h3>';
+                    echo '</a>';
+                echo '</div>';
+            echo '</div>';
+        echo '</div>';
+    endwhile;
+    echo '</div>';
+echo '</div>';
+endif;
