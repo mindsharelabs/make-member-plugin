4de82d9722c800cd1ae6a8df3fb4e35cf921ce10
James W Johnson <me@thejameswilliam.com>
2025-02-26 20:52:53 -0700

    migrated to mindshare simple events

--- inc/templates/make-upcoming-events.php
+++ inc/templates/make-upcoming-events.php
@@ -31,35 +31,42 @@ if(!$is_preview) :
     $settings = get_field('make_upcoming_events');
     $args = array();
     if($settings['event_categories']) {
-        $args['tax_query'][] = array(
-            'taxonomy' => 'tribe_events_cat',
+        $args['tax_query'] = array(
+            'taxonomy' => 'event_category',
             'field' => 'term-id',
             'terms' => $settings['event_categories'],
         );
     }
     $upcoming_events = make_get_upcoming_events($settings['num_events'] ?? 3, true, $args );
 
-
-    
+  
     echo '<div class="' . $className . '" id= "' . $id .'">';
         echo '<div class="row gy-3">';
         if($upcoming_events) :
             foreach($upcoming_events as $event => $title) :
-                $all_tickets = Tribe__Tickets__Tickets::get_all_event_tickets( $event );
+                $parent = get_post_parent( $event );
+                $start_date = new DateTime(get_post_meta($event, 'event_start_time_stamp', true));
+                $end_date = new DateTime(get_post_meta($event, 'event_end_time_stamp', true));
+                $product = get_post_meta($event, 'linked_product', true);
+                if(function_exists('wc_get_product')) :
+                    $product_obj = wc_get_product($product);
+                endif;
+                
                 echo '<div class="col-12 col-md-6 col-lg-3">';
                     echo '<div class="card d-flex flex-column h-100 justify-content-between">';
-                        if(has_post_thumbnail( $event ) && $settings['show_image']) {
-                            echo get_the_post_thumbnail( $event, 'horizontal-media-image', array('class' => 'card-img-top w-100') );
+                        if(has_post_thumbnail( $parent ) && $settings['show_image']) {
+                            echo get_the_post_thumbnail( $parent, 'horizontal-media-image', array('class' => 'card-img-top w-100') );
                         }
                         echo '<div class="card-body">';
-                            echo '<a href="' . get_the_permalink($event) . '">';
+                            echo '<a href="' . get_the_permalink($parent) . '">';
                                 echo '<h5 class="card-title text-center fw-bold">' . get_the_title($event) . '</h5>';
                             echo '</a>';    
-                            echo '<p class="card-tex text-center strong fw-bold">' . tribe_get_start_date($event, false, 'M j @ g:i a') . '</p>';
+                            echo '<p class="card-tex text-center strong fw-bold">' . $start_date->format('M j @ g:i a') . '</p>';
                             
                             if($settings['show_price']) :
+
                                 echo '<div class="price text-center mt-2 small">';
-                                    echo '<span class="label fw-bold">Cost: </span class="label">' . tribe_get_cost($event, true) . '</span>';
+                                    echo '<span class="label fw-bold">Cost: </span class="label">' . $product_obj->get_price_html() . '</span>';
                                 echo '</div>';
                             endif;
                             
@@ -71,7 +78,7 @@ if(!$is_preview) :
                             
                         echo '</div>';
                         echo '<div class="card-footer">';
-                            echo '<a href="' . get_the_permalink($event) . '" class="d-block my-2 btn btn-primary">' . ($settings['button_labels'] ? $settings['button_labels'] : 'Learn More') . '</a>';
+                            echo '<a href="' . get_the_permalink($parent) . '" class="d-block my-2 btn btn-primary">' . ($settings['button_labels'] ? $settings['button_labels'] : 'Learn More') . '</a>';
                         echo '</div>';
                     echo '</div>';
                 echo '</div>';
