0134080d92a84638343dd7f36bc2a5534531a330
James W. Johnson <me@thejameswilliam.com>
2026-05-07 11:46:34 -0600

    removed old ACF field.

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -196,61 +196,6 @@ function make_clean_bookable_resource($resource) {
 	update_post_meta($resource, '_wc_booking_availability', $booking_availability);
 };
 
-if( function_exists('acf_add_local_field_group') ):
-	acf_add_local_field_group(array(
-		'key' => 'group_63a1325d1933b',
-		'title' => 'Additional Event Options',
-		'fields' => array(
-			array(
-				'key' => 'field_63a1325d5221c',
-				'label' => 'Block Availability',
-				'name' => 'create_booking',
-				'aria-label' => '',
-				'type' => 'relationship',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'post_type' => array(
-					0 => 'bookable_resource',
-				),
-				'taxonomy' => '',
-				'filters' => array(
-					0 => 'search',
-				),
-				'return_format' => 'id',
-				'min' => '',
-				'max' => '',
-				'elements' => '',
-			),
-			
-		),
-		'location' => array(
-			array(
-				array(
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'events',
-				),
-			),
-		),
-		'menu_order' => 0,
-		'position' => 'side',
-		'style' => 'default',
-		'label_placement' => 'top',
-		'instruction_placement' => 'label',
-		'hide_on_screen' => '',
-		'active' => true,
-		'description' => '',
-		'show_in_rest' => 0,
-	));
-endif;		
-
-
 
 
 function make_event_has_available_tickets($event_id) {
